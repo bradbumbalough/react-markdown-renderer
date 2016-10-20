613635befbfbc426272a1fd0fe4dd26a8d8c656c
Jonathon Kelly <jonathon@insidersbyte.com>
2016-10-20 16:26:22 +0100

    :bug: don't spread markdown and options

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Remarkable from 'remarkable';
 
-export default function MarkdownRenderer(props) {
-    const remarkable = new Remarkable(props.options);
-    const html = remarkable.render(props.markdown);
+export default function MarkdownRenderer({ markdown, options = {}, ...props }) {
+    const remarkable = new Remarkable(options);
+    const html = remarkable.render(markdown);
 
     return (
         <div {...props} dangerouslySetInnerHTML={{ __html: html }} />
@@ -13,8 +13,4 @@ export default function MarkdownRenderer(props) {
 MarkdownRenderer.propTypes = {
     markdown: React.PropTypes.string.isRequired,
     options: React.PropTypes.shape({}),
-};
-
-MarkdownRenderer.defaultProps = {
-    options: {},
 };
