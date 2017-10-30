a894b2554529cd12e97f4e5b527f76687dc1d7fe
Brad Bumbalough <brad@teamgantt.com>
2017-10-30 16:10:41 -0400

    Allow manually adjusting rules through with props
    - The component will iterate through a `rules` prop object and pass the settings onto the remarkable instance.

--- src/index.js
+++ src/index.js
@@ -16,14 +16,55 @@ type PropsType = {
     quotes?: string,
     highlight?: (str: string, lang: string) => string,
   },
+  rules?: {
+    block?: {
+      enable?: array,
+      disable?: array,
+    },
+    core?: {
+      enable?: array,
+      disable?: array,
+    },
+    inline?: {
+      enable?: array,
+      disable?: array,
+    },
+  },
 };
 
 const MarkdownRenderer = ({
   markdown,
   options: { preset, ...options } = {},
+  rules,
   ...props
 }: PropsType) => {
   const remarkable = new Remarkable(preset || 'default', options);
+
+  /* rule is an object used to enable/disable rules.
+    {
+      core: {
+        enable: [ 'abbr' ]
+      },
+      inline: {
+        enable: [ 'ins', 'mark' ]
+      },
+      block: {
+        disable: [ 'table', 'footnote' ]
+      }
+    }
+  */
+  if (rules) {
+    Object.keys(rules).forEach(rule => {
+      if (remarkable[rule] && remarkable[rule].ruler) {
+        Object.keys(rules[rule]).forEach(flag => {
+          if (remarkable[rule].ruler[flag]) {
+            remarkable[rule].ruler[flag](rules[rule][flag]);
+          }
+        });
+      }
+    });
+  }
+
   const html = remarkable.render(markdown);
 
   return <div {...props} dangerouslySetInnerHTML={{ __html: html }} />;
